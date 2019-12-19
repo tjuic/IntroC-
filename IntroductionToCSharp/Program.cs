e35f74c3b80f95fbb40f1f6ee16b83255f72788b
atip chao <atip/chaogunhar@cookcountyil.gov>
2019-12-18 21:18:10 -0600

    first commit

---
+++ IntroductionToCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1 = 10;
+
+        bool IsNumber12 = n1 == 10 ? true : false;
+
+        Console.WriteLine("Number == 10 is {0}", IsNumber12);
+    }
+}
+
